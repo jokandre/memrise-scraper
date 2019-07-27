a9d832ce5d649c7b5c1f0b8ada38544579eb56f7
jokandre <jorgeandre93@yahoo.com.br>
2019-07-27 15:47:56 +0800

    -added pinyin according to ccdict -added added json formating

--- README.md
+++ README.md
@@ -1,17 +1,40 @@
 # memrise-scraper
 
-simple memrise scraper
+Simple memrise scraper
+
 
 ## Usage:
+Tags:
+
+--url or -u < url>
+
+--json or -j <True/False> :: default = True
+
+--pinyin or -p <True/False> :: default = True
+
 
 ### On Windows:
 ```
-py -3 memrise.py /course/68063/topik-in-30-days-intermediate-vocabulary/  >output.csv
+py -3 memrise.py -u /course/68063/topik-in-30-days-intermediate-vocabulary/  >output.csv
 ```
 
 ### On Linux:
 
 ```
-python3 memrise.py /course/68063/topik-in-30-days-intermediate-vocabulary/ | sort -u
+python3 memrise.py -u /course/68063/topik-in-30-days-intermediate-vocabulary/ | sort -u
 ```
+
+```
+python3 memrise.py -url /course/184712/practical-audio-visual-chinese-book-2-2/ > unit2.csv
+
+python3 memrise.py -url /course/196005/practical-audio-visual-chinese-book-3/ > unit3.csv
+
+python3 memrise.py -url /course/471083/practical-audio-visual-chinese-book-4-2/ > unit4.csv
+
+python3 memrise.py -url /course/408617/all-top-8829-traditional-mandarin-no-typing/ > mostfrequent.csv
+```
+
+### Json
+
+
 
--- memrise_scraper/memrise.py
+++ memrise_scraper/memrise.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
-COURSE_URL = "/course/977288/korean-grammar-in-use-11/"
-CARD_COLUMNS = ("col_a", "col_b")
-
+import argparse
 import codecs, sys
 import re
 import requests
 from bs4 import BeautifulSoup
+import json
+from pypinyin import pinyin, Style
 
+COURSE_URL = "/course/977288/korean-grammar-in-use-11/"
+CARD_COLUMNS = ("col_a", "col_b")
 
 def lazy_property(fn):
     """Decorator that makes a property lazy-evaluated.
@@ -28,6 +30,7 @@ def get_soup(url):
         url if url.strip().startswith("http") else "http://www.memrise.com" + url)
     soup = BeautifulSoup(res.text, "html.parser")
     return soup
+
 
 class Course(object):
 
@@ -86,6 +89,15 @@ class Course(object):
             yield cols
 
 
+class CourseDataHolder(object):
+    def __init__(self, course_title):
+        self.course_title = course_title
+        self.levels = []
+
+    def addLevel(self, level_name, items):
+        self.levels.append({'title':level_name, 'rows': items})
+
+
 def dump_course(*, course_url : str):
     """
     :course_url:   course URL
@@ -98,11 +110,83 @@ def dump_course(*, course_url : str):
             print('\t'.join(card))
 
 
-def main():
+def dump_course2json(*, course_url:str, to_add_pinyin:bool):
+    """
+    :course_url:   course URL
+    """
+    course = Course(course_url=course_url)
+    courseData = CourseDataHolder(course_url.split('/')[3])
+
+    for level_url, title in course.levels:
+        # print("*** %s (%s)" % (title, level_url))
+        temp = []
+        for card in course.cards(level_url=level_url):
+            # print('\t'.join(card))
+            cols = list(card)
+            row_obj = {}
+
+            if to_add_pinyin:
+                pin = pinyin(cols[0], style=Style.TONE3)
+                flat_pin = [item for sublist in pin for item in sublist]
+                flat_pin = cedict_tones(flat_pin)
+
+                row_obj = {'a': cols[0], 'b': cols[1], 'pron': ' '.join(flat_pin)}
+            else:
+                row_obj = {'a': cols[0], 'b': cols[1]}
+
+            temp.append(row_obj)
+        courseData.addLevel(title, temp)
+
+    # print(courseData.__dict__, len(courseData.levels))
+    print(json.dumps(courseData.__dict__, ensure_ascii=False))
+
+
+def cedict_tones(word):
+    """
+    Convert <neutral tone> to 5, ie, le -> le5
+    :return: retval, array of pinyin pronunciations
+    """
+    retval = []
+    for pron in word:
+        if not pron[-1].isdecimal():
+            pron = pron+'5'
+        retval.append(pron)
+    return retval
+
+
+def main(course_url='', to_json=True, to_add_pinyin=True):
     sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
-    course_url = COURSE_URL if len(sys.argv) < 2 else sys.argv[1]
-    dump_course(course_url=course_url)
+    # course_url = COURSE_URL if len(sys.argv) < 2 else sys.argv[1]
+
+    if to_json:
+        dump_course2json(course_url=course_url, to_add_pinyin=to_add_pinyin)
+    else:
+        dump_course(course_url=course_url)
 
 
 if __name__ == "__main__":
-    main()
+    P = argparse.ArgumentParser(
+        description='Scrape data from Memrise courses.'
+    )
+    P.add_argument(
+        '-u', '--url',
+        help='Partial url of the course, ie, /course/184712/practical-audio-visual-chinese-book-2-2/',
+        # required=True,
+        default='/course/184712/practical-audio-visual-chinese-book-2-2/'
+    )
+
+    P.add_argument(
+        '-j', '--json',
+        help='Use JSON formated output',
+        # required=True,
+        default=True
+    )
+
+    P.add_argument(
+        '-p', '--pinyin',
+        help='Add pinyin according to CCDICT dictionary',
+        default=True
+    )
+
+    ARGS = P.parse_args()
+    main(course_url=ARGS.url, to_json=ARGS.json, to_add_pinyin=ARGS.pinyin)
